2d71c9b9cdd084cda2eaaa70380921e20192ea34
Constantin Lungu <lungupconstantin@gmail.com>
2019-09-17 23:24:58 +0300

    Closes #12. Set up a Map MVP.
    Additional changes:
    - Set up fixtures to populate initial data
    - Model changes for consistency
    - Localization changes

--- seismic_site/map_app/models.py
+++ seismic_site/map_app/models.py
@@ -1,7 +1,6 @@
-from django.db import models
+from enum import Enum
 
-# Create your models here.
-
+from django.core.serializers.json import DjangoJSONEncoder
 from django.db import models
 
 
@@ -13,14 +12,21 @@ class CsvFile(models.Model):
         return self.name
 
 
+class SectorChoice(Enum):
+    s1 = 'Sector 1'
+    s2 = 'Sector 2'
+    s3 = 'Sector 3'
+    s4 = 'Sector 4'
+    s5 = 'Sector 5'
+    s6 = 'Sector 6'
+
+
+class SeismicCategoryChoice(Enum):
+    pass
+    # TODO Implement this and replace in model
+
 class Building(models.Model):
-    # SECTOR_CHOICES = (
-    #     ('s1', 'Sector 1'),
-    #     ('s2', 'Sector 2'),
-    #     ('s3', 'Sector 3'),
-    #     ('s4', 'Sector 4')
-    #     ('s5', 'Sector 5')
-    # )
+
     id_general = models.AutoField(primary_key=True)
     clasa_categorie = models.CharField(max_length=5)
     nr_pmb = models.IntegerField()
@@ -29,19 +35,26 @@ class Building(models.Model):
     loc = models.CharField(max_length=60)
     adresa = models.CharField(max_length=250)
     nr_postal = models.CharField(max_length=8)
-    sector = models.CharField(max_length=20, default='sector')
-    nr_Sector = models.IntegerField()
-    an_contruire = models.IntegerField
+    sector = models.CharField(max_length=20, choices=[(tag, tag.value) for tag in SectorChoice])
+    nr_sector = models.IntegerField(choices=[(i, i) for i in range(1, 7)])
+    an_construire = models.IntegerField(default=-1)
     regim_inaltime = models.CharField(max_length=50)
     nr_apart = models.IntegerField()
     arie_desfasurata = models.FloatField()
-    an_expertiza = models.IntegerField()
+    an_expertiza = models.IntegerField(default=-1)
     expert_atestat = models.CharField(max_length=100)
     obs = models.CharField(max_length=1000)
-    numar_cadastral = models.IntegerField()
+    nr_cadastral = models.IntegerField()
     nr_carte_funciara = models.CharField(max_length=50)
     actualizare_pmb = models.DateField()
     editare_admin = models.DateField()
 
     def __str__(self):
         return self.adresa
+
+
+class BuildingEncoder(DjangoJSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Building):
+            return str(obj)
+        return super().default(obj)
--- seismic_site/seismic_site/views.py
+++ seismic_site/seismic_site/views.py
@@ -1,5 +1,8 @@
+from django.core.serializers import serialize
 from django.shortcuts import render
+from map_app.models import Building, BuildingEncoder
 
 
 def index(request):
-    return render(request, "index.html")
+    return render(request, "index.html",
+                  context={"building_data": serialize('json', Building.objects.all(), cls=BuildingEncoder)})
