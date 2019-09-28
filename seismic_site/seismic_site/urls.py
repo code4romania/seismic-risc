bc5f78192be11af4ecc7a7422da34861c48c5e28
Tudor Amariei <tudoramariei@gmail.com>
2019-09-28 17:29:55 +0300

    Add ability to add attachments to page

--- seismic_site/seismic_site/urls.py
+++ seismic_site/seismic_site/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import index
 
 urlpatterns = [
@@ -24,4 +26,7 @@ urlpatterns = [
     path('account/', include('account.urls')),
     path('page/', include('cms.urls')),
     path('', index, name="index"),
-]
+] + static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT,
+)
