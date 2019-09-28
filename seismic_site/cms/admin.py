e18014332a5ff3a4d3f914ecf0a5298fe3e33abd
Tudor Amariei <tudoramariei@gmail.com>
2019-09-28 17:29:55 +0300

    Add ability to add attachments to page

--- seismic_site/cms/admin.py
+++ seismic_site/cms/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Page, Category
+from .models import Page, Category, Attachment
 
 
 @admin.register(Category)
@@ -8,8 +8,16 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', )
 
 
+
+class DocumentInline(admin.TabularInline):
+    model = Attachment
+
+
 @admin.register(Page)
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'updated_on', 'is_published')
     list_filter = ('is_published', )
     # prepopulated_fields = {"slug": ("title",)}
+    inlines = [
+        DocumentInline,
+    ]
