6e493cb8a12640c49fc91a5c76c445dc10e310ab
Iulius Curt <iulius.curt@gmail.com>
2019-12-14 12:16:51 +0200

    Cleanup old and unused API (some buggy)

--- seismic_site/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # post views
-    path('building/<int:id>/', views.building, name='building'),
-    path('buildings', views.buildings, name='buildings'),
-]
