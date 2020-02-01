64c94519ff3c9c36be0ad4717131051c510ed7ea
Alexandru Buhai <alexandru.buhai@sectorlabs.ro>
2020-02-01 17:13:09 +0200

    Add language changer functionality
    Issue: https://github.com/code4romania/seismic-risc/issues/97
    
    This pr focuses on the language change functionality for our project.
    I chose the Lingui Js library since it seems to offers the best features/ease of use.
    One thing that I still have to do in order to complete this feature is to add documantation and a how to guide.
    Right now the I pass the callbacks for changing the language using props. If we ever change to Redux or another state management system,
    this can be improved.
    
    Signed-off-by: Alexandru Buhai <alexandru.buhai@sectorlabs.ro>

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,26 +1,64 @@
-import React from "react"
-import { Layout, Menu, Button } from "antd"
-import logo from "../logo.svg"
+import React, { useState } from "react";
+import { Layout, Menu, Button } from "antd";
+import logo from "../logo.svg";
 import { Link } from "react-router-dom";
+import { Trans } from "@lingui/macro";
+import styles from "./header.module.css";
 
-const { Header } = Layout
+const { Header } = Layout;
 
-export default props => (
-  <Header style={{ background: "none" }}>
-    <div className="App-logo">
-      <Link to="/"><img src={logo} alt="Seismic Risc logo"></img></Link>
-    </div>
-    <Menu theme="light" mode="horizontal" className="App-menu">
-      <Menu.Item key="about">Despre</Menu.Item>
-      <Menu.Item key="guide">Ghid pentru asociații de proprietari</Menu.Item>
-      <Menu.Item key="legislation">Legislație</Menu.Item>
-      <Menu.Item key="bucharest">Bucureștiul vulnerabil</Menu.Item>
-      <Menu.Item key="contact">Contact</Menu.Item>
-      <Menu.Item key="add">
-        <Button type="primary" icon="plus-circle" size="large">
-          Adaugă o clădire
-        </Button>
-      </Menu.Item>
-    </Menu>
-  </Header>
-)
+export default ({ currentLanguage, languageChangeCallback }) => {
+  const [langText, setLangText] = useState("English");
+
+  return (
+    <Header style={{ background: "none" }}>
+      <div className="App-logo">
+        <img src={logo} alt="Seismic Risc logo"></img>
+      </div>
+      <Menu
+        style={{ float: "right" }}
+        theme="light"
+        mode="horizontal"
+        className="App-menu"
+        selectable={false}
+      >
+        <Menu.Item key="about">
+          <Trans>Despre</Trans>
+        </Menu.Item>
+        <Menu.Item key="guide">
+          <Trans>Ghid pentru asociații de proprietari</Trans>
+        </Menu.Item>
+        <Menu.Item key="legislation">
+          <Trans>Legislație</Trans>
+        </Menu.Item>
+        <Menu.Item key="bucharest">
+          <Trans>Bucureștiul vulnerabil</Trans>
+        </Menu.Item>
+        <Menu.Item key="contact">
+          <Trans>Contact</Trans>
+        </Menu.Item>
+        <Menu.Item key="add">
+          <Button type="primary" icon="plus-circle" size="large">
+            <Trans>Adaugă o clădire</Trans>
+          </Button>
+        </Menu.Item>
+        <Menu.Item key="lang">
+          <div
+            className={styles.languageChangeButton}
+            onClick={() => {
+              if (currentLanguage === "en") {
+                languageChangeCallback("ro");
+                setLangText("English");
+              } else {
+                languageChangeCallback("en");
+                setLangText("Romană");
+              }
+            }}
+          >
+            {langText}
+          </div>
+        </Menu.Item>
+      </Menu>
+    </Header>
+  );
+};
