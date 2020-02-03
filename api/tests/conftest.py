5ae9fd197989203da65b765489a6a6a69dfcd145
Iulius Curt <iulius.curt@gmail.com>
2020-02-03 11:23:21 +0200

    Fix most of Black's complaining About this one though, it's much more readable, way easier to quickly scan in the multi-line formatting -    return { -        "title": "Page title", -        "slug": "pg1", -        "content": "Contents" -    } +    return {"title": "Page title", "slug": "pg1", "content": "Contents"}

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -4,6 +4,7 @@ import pytest
 @pytest.fixture
 def api_client():
     from rest_framework.test import APIClient
+
     return APIClient()
 
 
--- api/tests/test_pages_api.py
+++ api/tests/test_pages_api.py
@@ -17,10 +17,12 @@ def test_page_details_get(basic_page_data, api_client):
     assert response.status_code == 200
     assert response.data["slug"] == page1.slug
 
+
 @pytest.mark.django_db
 def test_page_post_forbidden(basic_page_data, api_client):
-    response = api_client.post(base_url, basic_page_data, format='json')
+    response = api_client.post(base_url, basic_page_data, format="json")
     assert response.status_code == 403
+
 
 @pytest.mark.django_db
 def test_page_delete_forbidden(basic_page_data, api_client):
