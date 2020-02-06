b063482f5d0e902929348cefc054f5231e5ff053
Tudor Amariei <tudor.amariei@code4.ro>
2020-02-06 20:38:40 +0200

    Format remaining files

--- api/blog/admin.py
+++ api/blog/admin.py
@@ -5,17 +5,23 @@ from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
-    list_display = (
-        'title', 'slug', 'created', 'published', 'is_visible'
-    )
+    list_display = ("title", "slug", "created", "published", "is_visible")
     fieldsets = [
-        (None, {
-            'fields': (
-                'author', 'title', 'slug', 'image', 'tags', 'published',
-                'is_visible'
-            )
-        }),
-        ('Text', {'classes': ('full-width',), 'fields': ('text',)})
+        (
+            None,
+            {
+                "fields": (
+                    "author",
+                    "title",
+                    "slug",
+                    "image",
+                    "tags",
+                    "published",
+                    "is_visible",
+                )
+            },
+        ),
+        ("Text", {"classes": ("full-width",), "fields": ("text",)}),
     ]
 
 
