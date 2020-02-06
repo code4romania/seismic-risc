759c8ec38e72d75641aedca1194f358b2989c4d1
Tudor Amariei <tudor.amariei@code4.ro>
2020-02-06 20:02:27 +0200

    3

--- api/manage.py
+++ api/manage.py
@@ -11,9 +11,17 @@ def main():
         from configurations.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
+
+
+
+
+            'Couldn't import Django. Are you sure it's installed and '
             "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+                                                    
+                                                    
+                                                    
+                                                    
+            'forget to activate a virtual environment?'
         ) from exc
     execute_from_command_line(sys.argv)
 
