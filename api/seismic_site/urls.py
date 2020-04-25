19ffd16d2d98c214ce5e70642dc83e8cacfdcd58
Daniel Ursache Dogariu <contact@danniel.net>
2020-04-25 19:06:02 +0300

    Do not use i18n URLs for the API

--- api/seismic_site/urls.py
+++ api/seismic_site/urls.py
@@ -22,28 +22,35 @@ router.register(r"pages", PagesViewSet, basename="pages")
 router.register(r"posts", PostViewSet, basename="posts")
 
 
-urlpatterns = i18n_patterns(
-    path(
-        "admin/password_reset/",
-        auth_views.PasswordResetView.as_view(),
-        name="admin_password_reset",
-    ),
-    path(
-        "admin/password_reset/done/",
-        auth_views.PasswordResetDoneView.as_view(),
-        name="password_reset_done",
-    ),
-    path(
-        "admin/reset/<uidb64>/<token>/",
-        auth_views.PasswordResetConfirmView.as_view(),
-        name="password_reset_confirm",
-    ),
-    path(
-        "admin/reset/done/",
-        auth_views.PasswordResetCompleteView.as_view(),
-        name="password_reset_complete",
-    ),
-    path("admin/", admin.site.urls),
-    path("ckeditor/", include("ckeditor_uploader.urls")),
-    path("api/v1/", include(router.urls)),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = (
+    i18n_patterns(
+        # URL patterns which accept a language prefix
+        path(
+            "admin/password_reset/",
+            auth_views.PasswordResetView.as_view(),
+            name="admin_password_reset",
+        ),
+        path(
+            "admin/password_reset/done/",
+            auth_views.PasswordResetDoneView.as_view(),
+            name="password_reset_done",
+        ),
+        path(
+            "admin/reset/<uidb64>/<token>/",
+            auth_views.PasswordResetConfirmView.as_view(),
+            name="password_reset_confirm",
+        ),
+        path(
+            "admin/reset/done/",
+            auth_views.PasswordResetCompleteView.as_view(),
+            name="password_reset_complete",
+        ),
+        path("admin/", admin.site.urls),
+        path("ckeditor/", include("ckeditor_uploader.urls")),
+    )
+    + [
+        # URL patterns which do not use a language prefix
+        path("api/v1/", include(router.urls)),
+    ]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
