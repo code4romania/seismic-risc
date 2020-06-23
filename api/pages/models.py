5d7d74544dd95bbc5909a544cd9e2f75495081e6
Tudor Amariei <tudor.amariei@figshare.com>
2020-06-23 09:57:03 +0300

    Implement internationalisation for Django
    - update urls for admin area & text editor
    - translate all the strings from the app
    - adds an additional dependency: GNU gettext
    
    Co-authored-by: Daniel Ursache Dogariu <contact@danniel.net>

--- api/pages/models.py
+++ api/pages/models.py
@@ -2,18 +2,20 @@ from ckeditor.fields import RichTextField
 from django.db import models
 from django.utils import timezone
 from django.utils.text import slugify
+from django.utils.translation import gettext_lazy as _
 
 
 class Category(models.Model):
     name = models.CharField(
-        blank=False, null=False, max_length=150, unique=True
+        _("name"), blank=False, null=False, max_length=150, unique=True
     )
 
     def __str__(self):
         return "{}".format(self.name)
 
     class Meta:
-        verbose_name_plural = "categories"
+        verbose_name = _("category")
+        verbose_name_plural = _("categories")
 
 
 class Page(models.Model):
@@ -22,32 +24,45 @@ class Page(models.Model):
         blank=True,
         null=True,
         on_delete=models.SET_NULL,
-        help_text="Page category",
+        help_text=_("Page category"),
+        verbose_name=_("category"),
     )
-    title = models.CharField(blank=True, max_length=150, help_text="Page title")
+    title = models.CharField(
+        _("title"), blank=True, max_length=150, help_text=_("Page title")
+    )
     slug = models.SlugField(
+        _("slug"),
         unique=True,
         blank=True,
         null=False,
         max_length=150,
-        help_text="Unique URL slug (leave empty to auto-generate)",
+        help_text=_("Unique URL slug (leave empty to auto-generate)"),
     )
-    content = RichTextField()
+    content = RichTextField(_("content"))
     updated_on = models.DateTimeField(
+        _("updated on"),
         auto_now=timezone.now,
         blank=True,
         null=True,
         editable=False,
-        help_text="Last update time",
+        help_text=_("Last update time"),
     )
     publishing_date = models.DateTimeField(
-        blank=True, null=True, help_text="Public page publishing date"
+        _("publishing date"),
+        blank=True,
+        null=True,
+        help_text=_("Public page publishing date"),
     )
     is_published = models.BooleanField(
+        _("is published"),
         default=False,
         db_index=True,
-        help_text="Is this page visible on the website",
+        help_text=_("Is this page visible on the website"),
     )
+
+    class Meta:
+        verbose_name = _("page")
+        verbose_name_plural = _("pages")
 
     def __str__(self):
         return "{}".format(self.title)
@@ -78,15 +93,24 @@ class Attachment(models.Model):
         blank=True,
         null=False,
         on_delete=models.CASCADE,
-        help_text="Page attachment",
+        help_text=_("Page attachment"),
+        verbose_name=_("attachment"),
     )
-    name = models.CharField(max_length=150, help_text="Attachment name")
+    name = models.CharField(
+        _("name"), max_length=150, help_text=_("Attachment name")
+    )
     upload_date = models.DateTimeField(
-        auto_now=timezone.now, help_text="Attachment upload date"
+        _("upload date"),
+        auto_now=timezone.now,
+        help_text=_("Attachment upload date"),
     )
     uploaded_file = models.FileField(
-        upload_to="uploads/%Y/%m/%d/", max_length=100
+        _("uploaded file"), upload_to="uploads/%Y/%m/%d/", max_length=100
     )
+
+    class Meta:
+        verbose_name = _("attachment")
+        verbose_name_plural = _("attachments")
 
     def __str__(self):
         return "{}{}".format(self.uploaded_file.url, self.name)
--- api/seismic_site/urls.py
+++ api/seismic_site/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
@@ -21,29 +22,38 @@ router.register(r"pages", PagesViewSet, basename="pages")
 router.register(r"posts", PostViewSet, basename="posts")
 
 
-urlpatterns = [
-    path(
-        "admin/password_reset/",
-        auth_views.PasswordResetView.as_view(),
-        name="admin_password_reset",
-    ),
-    path(
-        "admin/password_reset/done/",
-        auth_views.PasswordResetDoneView.as_view(),
-        name="password_reset_done",
-    ),
-    path(
-        "admin/reset/<uidb64>/<token>/",
-        auth_views.PasswordResetConfirmView.as_view(),
-        name="password_reset_confirm",
-    ),
-    path(
-        "admin/reset/done/",
-        auth_views.PasswordResetCompleteView.as_view(),
-        name="password_reset_complete",
-    ),
-    path("admin/", admin.site.urls),
-    path("ckeditor/", include("ckeditor_uploader.urls")),
-    path("api/v1/buildings/search", building_search, name="building_search"),
-    path("api/v1/", include(router.urls)),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = (
+    i18n_patterns(
+        # URL patterns which accept a language prefix
+        path(
+            "admin/password_reset/",
+            auth_views.PasswordResetView.as_view(),
+            name="admin_password_reset",
+        ),
+        path(
+            "admin/password_reset/done/",
+            auth_views.PasswordResetDoneView.as_view(),
+            name="password_reset_done",
+        ),
+        path(
+            "admin/reset/<uidb64>/<token>/",
+            auth_views.PasswordResetConfirmView.as_view(),
+            name="password_reset_confirm",
+        ),
+        path(
+            "admin/reset/done/",
+            auth_views.PasswordResetCompleteView.as_view(),
+            name="password_reset_complete",
+        ),
+        path("admin/", admin.site.urls),
+        path("ckeditor/", include("ckeditor_uploader.urls")),
+    )
+    + [
+        # URL patterns which do not use a language prefix
+        path(
+            "api/v1/buildings/search", building_search, name="building_search"
+        ),
+        path("api/v1/", include(router.urls)),
+    ]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
