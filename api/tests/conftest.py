0d79c86a3055a12e327d552f9cf13ad823c09991
JustBeYou <mihailferaru2000@gmail.com>
2020-08-15 21:03:24 +0300

    tests: API tested entirely

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,14 +1,18 @@
 import pytest
 
-
 @pytest.fixture
 def api_client():
     from rest_framework.test import APIClient
 
     return APIClient()
 
+@pytest.fixture
+def test_user(db, django_user_model, basic_user_data):
+    return django_user_model.objects.create_user(**basic_user_data)
 
 @pytest.fixture
-def basic_page_data():
-    # Required fields for Page object
-    return {"title": "Page title", "slug": "pg1", "content": "Contents"}
+def basic_user_data():
+    return {
+        'username': 'testuser',
+        'password': 'testpassword',
+    }
