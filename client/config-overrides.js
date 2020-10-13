e639f9946e1481cdd617e46670eb7f82c54f5e4d
Tudor Amariei <tudor.amariei@figshare.com>
2020-10-13 19:43:25 +0300

    Manually bump less-loader to 7.0.2
    - breaking changes were introduced and now are fixed
    - closes #402

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,13 +1,15 @@
 const { override, fixBabelImports, addLessLoader } = require('customize-cra');
 
 module.exports = override(
-    fixBabelImports('import', {
-        libraryName: 'antd',
-        libraryDirectory: 'es',
-        style: true,
-    }),
-    addLessLoader({
-        javascriptEnabled: true,
-        modifyVars: { '@primary-color': '#EE741B' },
-    }),
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: true,
+  }),
+  addLessLoader({
+    lessOptions: {
+      javascriptEnabled: true,
+      modifyVars: { '@primary-color': '#EE741B' },
+    },
+  }),
 );
