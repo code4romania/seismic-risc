3cc5a46dde3d979654167240f35a772e82788fac
vladplesu <vladp1987@gmail.com>
2021-02-05 22:38:34 +0200

    close #317 make use of the API to get search data add context provider add reducer add search handlers in SearchFragment add SearchResults component to show results

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useCallback } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { I18nProvider } from '@lingui/react';
+import { AppProvider } from './context';
 
 import Home from './containers/home';
 import Guide from './containers/guide';
@@ -22,28 +23,30 @@ const App = () => {
     setLanguage(languageCode);
   }, []);
   return (
-    <I18nProvider language={language} catalogs={catalogs}>
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home language={language} languageChangeCallback={languageChangeCallback} />
-          </Route>
-          <Route path="/ghid">
-            <Guide />
-          </Route>
-          <Route path="/blog">
-            <Blog />
-          </Route>
-          <Route path="/termeni-si-conditii">
-            <Terms />
-          </Route>
-          <Route path="/politica-de-confidentialitate">
-            <Policy />
-          </Route>
-        </Switch>
-        <Footer />
-      </Router>
-    </I18nProvider>
+    <AppProvider>
+      <I18nProvider language={language} catalogs={catalogs}>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Home language={language} languageChangeCallback={languageChangeCallback} />
+            </Route>
+            <Route path="/ghid">
+              <Guide />
+            </Route>
+            <Route path="/blog">
+              <Blog />
+            </Route>
+            <Route path="/termeni-si-conditii">
+              <Terms />
+            </Route>
+            <Route path="/politica-de-confidentialitate">
+              <Policy />
+            </Route>
+          </Switch>
+          <Footer />
+        </Router>
+      </I18nProvider>
+    </AppProvider>
   );
 };
 
--- client/src/containers/home/SearchFragment.js
+++ client/src/containers/home/SearchFragment.js
@@ -1,25 +1,44 @@
-import React from 'react';
-import { Row, Col, Input, Typography } from 'antd';
+import React, { useEffect } from 'react';
+import { Row, Col, Input, Typography, message } from 'antd';
+
+import { useGlobalContext } from '../../context';
 
 const { Search } = Input;
 const { Title, Paragraph } = Typography;
 
-export default () => (
-  <Row type="flex" justify="center" align="top" style={{ marginTop: '2rem', marginBottom: '2rem' }}>
-    <Col span={16}>
-      <Paragraph ellipsis={{ rows: 3, expandable: true }}>
-        Many desktop publishing packages and web page editors now use Lorem Ipsum as their default
-        model text, and a search for 'lorem ipsum' will uncover many web sites still in their
-        infancy. Various versions have evolved over the years, sometimes by accident, sometimes on
-        purpose (injected humour and the like).
-      </Paragraph>
-      <Title level={3}>Verifică aici dacă o clădire se află pe lista de risc seismic:</Title>
-      <Search
-        loading={false}
-        placeholder="Scrie adresa clădirii aici"
-        onSearch={(value) => value}
-        style={{ width: '80%' }}
-      />
-    </Col>
-  </Row>
-);
+export default () => {
+  const { searchBuildings, searchError, searchInput, onSearchInputChange } = useGlobalContext();
+
+  useEffect(() => {
+    if (searchError) {
+      message.warning(searchError);
+    }
+  });
+
+  return (
+    <Row
+      type="flex"
+      justify="center"
+      align="top"
+      style={{ marginTop: '2rem', marginBottom: '2rem' }}
+    >
+      <Col span={16}>
+        <Paragraph ellipsis={{ rows: 3, expandable: true }}>
+          Many desktop publishing packages and web page editors now use Lorem Ipsum as their default
+          model text, and a search for 'lorem ipsum' will uncover many web sites still in their
+          infancy. Various versions have evolved over the years, sometimes by accident, sometimes on
+          purpose (injected humour and the like).
+        </Paragraph>
+        <Title level={3}>Verifică aici dacă o clădire se află pe lista de risc seismic:</Title>
+        <Search
+          value={searchInput}
+          loading={false}
+          placeholder="Scrie adresa clădirii aici"
+          onChange={(e) => onSearchInputChange(e.target.value)}
+          onSearch={(value) => searchBuildings(value)}
+          style={{ width: '80%' }}
+        />
+      </Col>
+    </Row>
+  );
+};
