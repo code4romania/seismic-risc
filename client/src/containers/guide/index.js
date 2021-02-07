d66d4f2f95a063c772c508d7e2e02c933040df1d
Gabriel Majeri <gabriel.majeri6@gmail.com>
2021-02-07 16:34:34 +0200

    Improve guide page UX

--- client/src/containers/guide/index.js
+++ client/src/containers/guide/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { Row, Col } from 'antd';
+import { useLocation } from 'react-router-dom';
+import qs from 'qs';
+import { Row, Col, Button } from 'antd';
 import Layout from '../../components/Layout';
 import PdfViewer from './PdfViewer';
 
 const GUIDE_PDF_URL = '/assets/documents/Toolkit Asociatii de proprietari v2018.pdf';
 
 const Guide = () => {
+  const { search } = useLocation();
+  const { page } = qs.parse(search, { ignoreQueryPrefix: true });
+  let pageNumber = parseInt(page, 10);
+  if (Number.isNaN(pageNumber)) {
+    pageNumber = 1;
+  }
   return (
     <Layout>
       <div className="page">
@@ -17,8 +25,13 @@ const Guide = () => {
           style={{ marginTop: '2rem', marginBottom: '2rem' }}
         >
           <Col>
-            <PdfViewer file={GUIDE_PDF_URL} />
+            <PdfViewer file={GUIDE_PDF_URL} defaultPageNumber={pageNumber} />
           </Col>
+        </Row>
+        <Row type="flex" justify="center">
+          <Button type="primary" icon="download" href={GUIDE_PDF_URL} download>
+            Descarcă
+          </Button>
         </Row>
       </div>
     </Layout>
