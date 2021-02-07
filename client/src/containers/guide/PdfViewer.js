8fd36df2ed105de7998d9a00a1acddd0262f41b2
Gabriel Majeri <gabriel.majeri6@gmail.com>
2021-02-07 16:34:34 +0200

    Improve guide page UX

--- client/src/containers/guide/PdfViewer.js
+++ client/src/containers/guide/PdfViewer.js
@@ -1,6 +1,6 @@
-import React, { useState, useLayoutEffect } from 'react';
+import React, { useState, useLayoutEffect, useEffect } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
-import { Row, Col, Button } from 'antd';
+import { Row, Button } from 'antd';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
@@ -26,43 +26,65 @@ const useWindowSize = () => {
 const MAX_PDF_PAGE_WIDTH = 500;
 
 // Based on https://github.com/wojtekmaj/react-pdf/wiki/Recipes#display-single-page-with-navigation
-const PdfViewer = ({ file }) => {
+const PdfViewer = ({ file, defaultPageNumber = 1 }) => {
   const windowSize = useWindowSize();
   const pageWidth = Math.min(MAX_PDF_PAGE_WIDTH, windowSize.width);
 
   const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
+  const [pageNumber, setPageNumber] = useState(defaultPageNumber);
 
   const handleLoadSuccess = (pdf) => {
     setNumPages(pdf.numPages);
-    setPageNumber(1);
   };
 
   const changePage = (offset) => setPageNumber((num) => num + offset);
   const previousPage = () => changePage(-1);
   const nextPage = () => changePage(+1);
 
+  const handleKeyDown = (e) => {
+    switch (e.key) {
+      case 'ArrowLeft':
+        if (pageNumber > 1) {
+          previousPage();
+        }
+        break;
+      case 'ArrowRight':
+        if (pageNumber < numPages) {
+          nextPage();
+        }
+        break;
+      default:
+        break;
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  });
+
   return (
     <>
-      <Document file={file} onLoadSuccess={handleLoadSuccess}>
-        <Page pageNumber={pageNumber} width={pageWidth} />
-      </Document>
+      <div style={{ width: pageWidth, minHeight: pageWidth * 1.5 }}>
+        <Document file={file} onLoadSuccess={handleLoadSuccess}>
+          <Page pageNumber={pageNumber} width={pageWidth} />
+        </Document>
+      </div>
       {numPages && (
         <>
           <p style={{ textAlign: 'center' }}>
             Pagina {pageNumber} din {numPages}
           </p>
           <Row type="flex" justify="center">
-            <Col span={8}>
-              <Button disabled={pageNumber <= 1} onClick={previousPage}>
-                Pagina anterioară
-              </Button>
-            </Col>
-            <Col span={8}>
-              <Button type="Primary" disabled={pageNumber >= numPages} onClick={nextPage}>
-                Pagina următoare
-              </Button>
-            </Col>
+            <Button disabled={pageNumber <= 1} onClick={previousPage} style={{ marginRight: 16 }}>
+              Pagina anterioară
+            </Button>
+            <Button type="Primary" disabled={pageNumber >= numPages} onClick={nextPage}>
+              Pagina următoare
+            </Button>
           </Row>
         </>
       )}
