f1527b6438ca1ced6d889d1f886d45357f035ced
Tudor Amariei <tudor.amariei@code4.ro>
2021-02-23 17:28:14 +0200

    Change stage deployment to not include client
    - build only db and api
    - run migrations

--- deploy/pull.sh
+++ deploy/pull.sh
@@ -1,6 +1,8 @@
 cd /root/seismic-risc
 git pull
-docker-compose build
-docker-compose down
-docker-compose up -d
+docker-compose build db api
 
+make migrate
+
+docker-compose down
+docker-compose up -d db api
