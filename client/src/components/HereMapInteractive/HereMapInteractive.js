a628828d5ba830e323f18bd6f6fe7dc7754b3631
Anna Kiss <annacs94@gmail.com>
2021-02-24 12:09:15 +0200

    add clustering to map

---
+++ client/src/components/HereMapInteractive/HereMapInteractive.js
@@ -0,0 +1,69 @@
+import React, { useLayoutEffect, useRef, useState } from 'react';
+import BuildingDetails from '../BuildingDetails';
+import SearchResults from '../SearchResults';
+
+import { useGlobalContext } from '../../context';
+import useDecoratedClusteredMap from '../../hooks/map/useDecoratedClusteredMap';
+
+const { H } = window;
+
+const HereMapInteractive = (props) => {
+  const { apikey, points } = props;
+
+  const [currentMap, setCurrentMap] = useState(undefined);
+
+  const mapRef = useRef(null);
+
+  const { onHereMapLoaded } = useGlobalContext();
+
+  const { isDrawerVisible, buildingDetails, setDrawerVisible } = useDecoratedClusteredMap(
+    currentMap,
+    points,
+  );
+
+  useLayoutEffect(() => {
+    if (!mapRef.current) return;
+    const platform = new H.service.Platform({ apikey });
+    const layer = platform.createDefaultLayers();
+
+    const map = new H.Map(mapRef.current, layer.vector.normal.map, {
+      center: { lat: 44.4268, lng: 26.1025 },
+      zoom: 10,
+    });
+
+    window.addEventListener('resize', () => map.getViewPort().resize());
+
+    const events = new H.mapevents.MapEvents(map);
+    // eslint-disable-next-line no-unused-vars
+    const behavior = new H.mapevents.Behavior(events);
+    /* eslint-disable-next-line no-unused-vars, new-cap */
+    const ui = new H.ui.UI.createDefault(map, layer);
+
+    setCurrentMap(map);
+    onHereMapLoaded(map);
+
+    // eslint-disable-next-line consistent-return
+    return () => map.dispose();
+  }, [apikey]);
+
+  return (
+    <div>
+      {apikey && (
+        <div
+          className="map-container"
+          ref={mapRef}
+          style={{ width: '100%', height: '400px', background: 'grey' }}
+        >
+          <BuildingDetails
+            visible={isDrawerVisible}
+            onClose={() => setDrawerVisible(false)}
+            details={buildingDetails}
+          />
+          <SearchResults />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default HereMapInteractive;
---
+++ client/src/hooks/map/ClusterLayerBuilder.js
@@ -0,0 +1,43 @@
+const { H } = window;
+
+const svgMarkup =
+  '<div class="marker">' +
+  '  <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">' +
+  '    <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/><path d="M0 0h24v24H0z" fill="none"/>' +
+  '  </svg>' +
+  '</div>';
+
+export default {
+  buildClusterLayer: (data, onMarkerClick) => {
+    const dataPoints = data.map((item) => {
+      return new H.clustering.DataPoint(item.lat, item.lng, 1, item);
+    });
+    const icon = new H.map.DomIcon(svgMarkup);
+    const clusteredDataProvider = new H.clustering.Provider(dataPoints, {
+      clusteringOptions: {
+        eps: 32,
+        minWeight: 3,
+      },
+    });
+    const defaultTheme = clusteredDataProvider.getTheme();
+    const customTheme = {
+      getClusterPresentation: (cluster) => {
+        const clusterMarker = defaultTheme.getClusterPresentation.call(defaultTheme, cluster);
+        return clusterMarker;
+      },
+      getNoisePresentation: (noisePoint) => {
+        const noiseMarker = new H.map.DomMarker(noisePoint.getPosition(), {
+          icon,
+          min: noisePoint.getMinZoom(),
+        });
+        noiseMarker.setData(noisePoint);
+        return noiseMarker;
+      },
+    };
+    clusteredDataProvider.setTheme(customTheme);
+    clusteredDataProvider.addEventListener('tap', (event) => {
+      onMarkerClick(event.target.getData());
+    });
+    return new H.map.layer.ObjectLayer(clusteredDataProvider);
+  },
+};
