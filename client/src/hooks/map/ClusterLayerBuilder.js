cdf66129670c0902f2e9c69ce26dc1490de39ab2
Mihai Dinu <Mihai.Dinu@harman.com>
2021-03-04 17:50:56 +0200

    adapt map colored pins to clustering feature

--- client/src/hooks/map/ClusterLayerBuilder.js
+++ client/src/hooks/map/ClusterLayerBuilder.js
@@ -1,18 +1,27 @@
 const { H } = window;
 
-const markerSvg =
-  '  <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">' +
-  '    <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/><path d="M0 0h24v24H0z" fill="none"/>' +
-  '  </svg>';
-const domIconMarkup = `<div class="marker">${markerSvg}</div>`;
-const selectedDomIconMarkup = `<div class="marker selected">${markerSvg}</div>`;
+const makeMarkerIcon = (category) => {
+  let className = 'marker selected';
+  if (category) className = `marker-${category.toLowerCase()}`;
+
+  const markerSvg =
+    `<div class="${className}">` +
+    `<svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">` +
+    `<circle style="" cx="12.43" cy="8.37" r="8.107"/>` +
+    `<path style="" transform="matrix(0.033909, 0.021505, -0.022542, 0.035546, 11.993942, -9.750627)" d="M 176 536 A 303.401 303.401 0 0 1 452 358.599 L 452 662 Z"/>` +
+    `<text style="fill: rgb(255, 249, 249); font-size: 7.9px; white-space: pre; text-anchor: middle;" x="12" y="11.114">${
+      category ? category.toUpperCase() : ''
+    }</text>` +
+    `</svg></div>`;
+
+  return new H.map.DomIcon(markerSvg);
+};
 
 export default {
   buildClusterLayer: (data, onClusterClick, onNoiseClick) => {
     const dataPoints = data.map((item) => {
       return new H.clustering.DataPoint(item.lat, item.lng, 1, item);
     });
-    const icon = new H.map.DomIcon(domIconMarkup);
     const clusteredDataProvider = new H.clustering.Provider(dataPoints, {
       clusteringOptions: {
         eps: 32,
@@ -26,6 +35,8 @@ export default {
         return clusterMarker;
       },
       getNoisePresentation: (noisePoint) => {
+        const riskCategory = noisePoint.getData().risk_category;
+        const icon = makeMarkerIcon(riskCategory);
         const noiseMarker = new H.map.DomMarker(noisePoint.getPosition(), {
           icon,
           min: noisePoint.getMinZoom(),
@@ -45,11 +56,9 @@ export default {
     return new H.map.layer.ObjectLayer(clusteredDataProvider);
   },
   unhighlightMarker: (marker) => {
-    const icon = new H.map.DomIcon(domIconMarkup);
-    marker.setIcon(icon);
+    marker.setIcon(makeMarkerIcon(marker.getData().getData().risk_category));
   },
   highlightMarker: (marker) => {
-    const selectedIcon = new H.map.DomIcon(selectedDomIconMarkup);
-    marker.setIcon(selectedIcon);
+    marker.setIcon(makeMarkerIcon());
   },
 };
