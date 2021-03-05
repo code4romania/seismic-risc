47eee4970c83122bc5903177af9a4d11c616a285
Tudor Amariei <tudor.amariei@code4.ro>
2021-03-05 13:21:01 +0200

    Change staging deploy configuration

--- deploy/pull.sh
+++ deploy/pull.sh
@@ -1,8 +1,10 @@
 cd /root/seismic-risc
 git pull
+
 docker-compose build db api
 
-make migrate
+docker-compose run --rm api "./manage.py migrate"
+docker-compose run --rm api "./manage.py compilemessages"
 
 docker-compose down
 docker-compose up -d db api
