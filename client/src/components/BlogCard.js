f2d9dd72b83ef07ea2a8233b55025e132d493740
Mihai Dinu <Mihai.Dinu@harman.com>
2021-03-16 13:32:20 +0200

    rework

--- client/src/components/BlogCard.js
+++ client/src/components/BlogCard.js
@@ -1,15 +1,18 @@
 import { React } from 'react';
+import { Link } from 'react-router-dom';
 import { Card, Typography } from 'antd';
 
 const { Title } = Typography;
 
-export default ({ title, imageUrl }) => {
+export default ({ title, imageUrl, slug }) => {
   return (
-    <Card className="blog-post-card">
-      <Title level={4}>{title}</Title>
-      <div className="img-container">
-        <img src={imageUrl} alt={title} />
-      </div>
-    </Card>
+    <Link to={`/blog/${slug}`}>
+      <Card className="blog-post-card">
+        <Title level={4}>{title}</Title>
+        <div className="img-container">
+          <img src={imageUrl} alt={title} />
+        </div>
+      </Card>
+    </Link>
   );
 };
