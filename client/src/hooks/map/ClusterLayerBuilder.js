ff710d6672fed1168719b887cb38f5defc6f4c64
Anna Kiss <annacs94@gmail.com>
2021-05-12 13:49:13 +0300

    add map input for the form

--- client/src/hooks/map/ClusterLayerBuilder.js
+++ client/src/hooks/map/ClusterLayerBuilder.js
@@ -1,21 +1,6 @@
+import HereMapDomIconFactory from '../../components/HereMapDomIconFactory';
+
 const { H } = window;
-
-const makeMarkerIcon = (category) => {
-  let className = 'marker selected';
-  if (category) className = `marker-${category.toLowerCase()}`;
-
-  const markerSvg =
-    `<div class="${className}">` +
-    `<svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">` +
-    `<circle style="" cx="12.43" cy="8.37" r="8.107"/>` +
-    `<path style="" transform="matrix(0.033909, 0.021505, -0.022542, 0.035546, 11.993942, -9.750627)" d="M 176 536 A 303.401 303.401 0 0 1 452 358.599 L 452 662 Z"/>` +
-    `<text style="fill: rgb(255, 249, 249); font-size: 7.9px; white-space: pre; text-anchor: middle;" x="12" y="11.114">${
-      category ? category.toUpperCase() : ''
-    }</text>` +
-    `</svg></div>`;
-
-  return new H.map.DomIcon(markerSvg);
-};
 
 export default {
   buildClusterLayer: (data, onClusterClick, onNoiseClick) => {
@@ -36,7 +21,7 @@ export default {
       },
       getNoisePresentation: (noisePoint) => {
         const riskCategory = noisePoint.getData().risk_category;
-        const icon = makeMarkerIcon(riskCategory);
+        const icon = HereMapDomIconFactory.makeMarkerIcon(riskCategory);
         const noiseMarker = new H.map.DomMarker(noisePoint.getPosition(), {
           icon,
           min: noisePoint.getMinZoom(),
@@ -56,9 +41,9 @@ export default {
     return new H.map.layer.ObjectLayer(clusteredDataProvider);
   },
   unhighlightMarker: (marker) => {
-    marker.setIcon(makeMarkerIcon(marker.getData().getData().risk_category));
+    marker.setIcon(HereMapDomIconFactory.makeMarkerIcon(marker.getData().getData().risk_category));
   },
   highlightMarker: (marker) => {
-    marker.setIcon(makeMarkerIcon());
+    marker.setIcon(HereMapDomIconFactory.makeMarkerIcon());
   },
 };
