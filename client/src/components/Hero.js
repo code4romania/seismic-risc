cf3ace085141ce0df2f38a1dd875361254b8a56d
vladplesu <vladp1987@gmail.com>
2021-05-22 23:52:18 +0300

    update about page to new specs modify footer to stick to bottom

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -6,11 +6,11 @@ const { Title } = Typography;
 export default ({ heroImage, title, children }) => {
   return (
     <div className="hero-container">
-      <Row className="container hero-body" type="flex" align="middle">
+      <Row className="container hero-body" type="flex" align="middle" justify="space-between">
         <Col sm={24} md={12}>
           <img src={heroImage} />
         </Col>
-        <Col sm={24} md={12}>
+        <Col sm={24} md={11}>
           <Title level={2} className="hero-title">
             {title}
           </Title>
--- client/src/containers/about/FirstParagraphsFragment.js
+++ client/src/containers/about/FirstParagraphsFragment.js
@@ -6,7 +6,7 @@ const { Paragraph } = Typography;
 
 export default () => {
   return (
-    <Row type="flex" justify="space-around" style={{ textAlign: 'left' }}>
+    <Row type="flex" justify="space-around" style={{ marginTop: '4rem', textAlign: 'left' }}>
       <Col span={24}>
         <Paragraph style={{ paddingBottom: '2rem' }}>
           <Trans id="about.first_paragraph" />
