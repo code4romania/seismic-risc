e6dae2807149d1a5dd3b4e5add67b81009d7ded4
vladplesu <vladp1987@gmail.com>
2021-05-25 11:58:29 +0300

    update stats cards to new specs (#663)
    * update stats cards to new specs

--- client/src/containers/home/StatisticFragment.js
+++ client/src/containers/home/StatisticFragment.js
@@ -4,32 +4,30 @@ import { Trans } from '@lingui/macro';
 
 import { ReactComponent as PeopleRisk } from '../../images/user-solid.svg';
 import { ReactComponent as EvaluatedBuildings } from '../../images/house-damage-solid.svg';
-import { ReactComponent as ConsolidatedBuildings } from '../../images/home-solid.svg';
+import { ReactComponent as ConsolidatedBuildings } from '../../images/wrench-solid.svg';
+import { ReactComponent as StatsIconBackground } from '../../images/stats-icon-background.svg';
 
 import config from '../../config';
 
 const { STATISTICS_URL } = config;
 
-const StatisticCard = ({ title, value, icon }) => (
-  <Card className="statistics-card" size="small">
-    <Row type="flex" justify="space-between" align="middle">
-      <Col span={4}>{icon}</Col>
-      <Col span={18}>
-        {value > 0 ? (
-          <>
-            <h3>{value}</h3>
-            <h4>{title}</h4>
-          </>
-        ) : (
-          <>
-            <h3>{title}</h3>
-            <h4>
-              <Trans>Information being updated</Trans>
-            </h4>
-          </>
-        )}
-      </Col>
-    </Row>
+const StatisticCard = ({ title, value, icon, type = '' }) => (
+  <Card className={`stats-card ${type}`} size="small">
+    {value > 0 ? (
+      <>
+        <h3 className="stats-heading">{value}</h3>
+        <h4>{title}</h4>
+      </>
+    ) : (
+      <>
+        <h3 className="stats-heading">{title}</h3>
+        <h4>
+          <Trans>Information being updated</Trans>
+        </h4>
+      </>
+    )}
+    <StatsIconBackground className={`stats-background ${type}`} />
+    <div className="stats-icon">{icon}</div>
   </Card>
 );
 
@@ -67,6 +65,7 @@ export default () => {
           title={<Trans>People under risk</Trans>}
           value={state.statistics.people_under_risk}
           icon={<PeopleRisk />}
+          type="people-under-risk"
         />
       </Col>
       <Col xs={24} md={8}>
@@ -74,6 +73,7 @@ export default () => {
           title={<Trans>Evaluated buildings</Trans>}
           value={state.statistics.evaluated_buildings}
           icon={<EvaluatedBuildings />}
+          type="evaluated-buildings"
         />
       </Col>
       <Col xs={24} md={8}>
@@ -81,6 +81,7 @@ export default () => {
           title={<Trans>Consolidated buildings</Trans>}
           value={state.statistics.consolidated_buildings}
           icon={<ConsolidatedBuildings />}
+          type="consolidated-buildings"
         />
       </Col>
     </Row>
