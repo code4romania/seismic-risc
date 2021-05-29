5b31ea497b7a821ba665382e1b685fd51d4d586e
vladplesu <vladp1987@gmail.com>
2021-05-29 22:47:43 +0300

    update not found page to match new design specs

--- client/src/containers/404/404.js
+++ client/src/containers/404/404.js
@@ -1,42 +1,12 @@
 import React from 'react';
-import { Row, Col, Typography } from 'antd';
-import { Trans } from '@lingui/macro';
 import Layout from '../../components/Layout';
-import logo from '../../logo-big.svg';
-
-const { Title, Paragraph } = Typography;
+import HeroFragment from './HeroFragment';
+import BlogPreviewFragment from './BlogPreviewFragment';
 
 const NotFound = () => {
-  const fontStyle = {
-    fontSize: '16px',
-  };
-
   return (
-    <Layout>
-      <div className="page">
-        <Row
-          type="flex"
-          justify="center"
-          gutter={0}
-          style={{ marginTop: '10%', marginBottom: '10%' }}
-        >
-          <Col lg={9} sm={24}>
-            <img src={logo} width="100%" />
-          </Col>
-          <Col lg={9} sm={24} style={{ paddingTop: '4%' }}>
-            <Title level={1} underline>
-              <Trans id="404.title" />
-            </Title>
-            <Paragraph style={fontStyle}>
-              <Trans id="404.sorry_message" />
-            </Paragraph>
-            <Paragraph style={fontStyle}>
-              <Trans id="404.contact_message" />
-              <a href="mailto:contact@code4.ro"> contact@code4.ro</a>.
-            </Paragraph>
-          </Col>
-        </Row>
-      </div>
+    <Layout hero={<HeroFragment />}>
+      <BlogPreviewFragment />
     </Layout>
   );
 };
