d98e204fd519975212a3c3ecc414aacad0c44439
Ciprian Platica <platica.ciprian@gmail.com>
2021-08-18 02:56:23 +0300

    Update building details designs

--- client/src/components/BuildingDetails/index.js
+++ client/src/components/BuildingDetails/index.js
@@ -1,82 +1,77 @@
 import React from 'react';
-import { Typography, Row, Col, Icon } from 'antd';
+import { Row, Col, Icon, Button, Descriptions, Divider } from 'antd';
 import { Trans } from '@lingui/macro';
-
-const { Title } = Typography;
+import { InfoCircleFilled } from '@ant-design/icons';
 
 export default function BuildingDetails(props) {
   const { onClose, details } = props;
   return (
     <Row className="building-details">
-      <Row>
-        <Title className="building-details-title" level={3}>
-          <Col lg={1} span={0}>
-            <Icon type="environment" />
-          </Col>
-          <Col lg={{ span: 21, offset: 1 }} span={23}>
-            <Trans>Building Info</Trans>
-          </Col>
-          <Col span={1}>
-            <Icon type="close" onClick={onClose} />
-          </Col>
-        </Title>
-      </Row>
       {details ? (
-        <div>
-          <Row>
-            <Col lg={{ span: 13, offset: 2 }} span={13}>
-              <Trans>Address</Trans>:
-            </Col>
-            <Col lg={9} span={11}>
-              {details.address && `${details.address} `}
-              {details.street_number && (
+        <Row type="flex" gutter={16} style={{ flexWrap: 'nowrap' }}>
+          <Col>
+            <Icon type="environment" style={{ marginTop: '8px', color: '#EE4036' }} />
+          </Col>
+          <Col>
+            <Descriptions
+              column={1}
+              title={
                 <>
-                  <Trans>no.</Trans> {details.street_number}
+                  {details.address}
+                  {details.street_number && (
+                    <>
+                      <Trans> no.</Trans> {details.street_number}
+                    </>
+                  )}
+                  <Button type="link">
+                    <Trans>(see images)</Trans>
+                  </Button>
                 </>
-              )}
-            </Col>
-          </Row>
-          <Row>
-            <Col lg={{ span: 13, offset: 2 }} span={13}>
-              <Trans>Construction Year</Trans>:
-            </Col>
-            <Col lg={9} span={11}>
-              {details.year_built}
-            </Col>
-          </Row>
-          <Row>
-            <Col lg={{ span: 13, offset: 2 }} span={13}>
-              <Trans>Height regime</Trans>:
-            </Col>
-            <Col lg={9} span={11}>
-              {details.height_regime}
-            </Col>
-          </Row>
-          <Row>
-            <Col lg={{ span: 13, offset: 2 }} span={13}>
-              <Trans>Risk category</Trans>:
-            </Col>
-            <Col lg={9} span={11}>
-              {details.risk_category}
-            </Col>
-          </Row>
-          <Row gutter={1}>
-            <Col lg={{ span: 13, offset: 2 }} span={13}>
-              <Trans>Examination year</Trans>:
-            </Col>
-            <Col lg={9} span={11}>
-              {details.examination_year}
-            </Col>
-          </Row>
-          <Row>
-            <Col lg={{ span: 13, offset: 2 }} span={13}>
-              <Trans>Certified expert name</Trans>:
-            </Col>
-            <Col lg={9} span={11}>
-              {details.certified_expert}
-            </Col>
-          </Row>
-        </div>
+              }
+            >
+              <Descriptions.Item label={<Trans>Construction Year</Trans>}>
+                {details.year_built}
+              </Descriptions.Item>
+              <Descriptions.Item label={<Trans>Height regime</Trans>}>
+                {details.height_regime}
+              </Descriptions.Item>
+              <Descriptions.Item label={<Trans>Risk category</Trans>}>
+                {details.risk_category}
+              </Descriptions.Item>
+              <Descriptions.Item label={<Trans>Examination year</Trans>}>
+                {details.examination_year}
+              </Descriptions.Item>
+              <Descriptions.Item label={<Trans>Certified expert name</Trans>}>
+                {details.certified_expert}
+              </Descriptions.Item>
+            </Descriptions>
+            <div style={{ overflow: 'hidden' }}>
+              <Divider className="buildingDetails__divider" orientation="left">
+                <InfoCircleFilled style={{ color: '#EE4036' }} />
+              </Divider>
+            </div>
+            <Row type="flex" justify="space-between" gutter={16} style={{ flexWrap: 'nowrap' }}>
+              <Col className="buildingDetails__contribute">
+                <Trans>Do you have more details about this building?</Trans>
+              </Col>
+              <Col>
+                <Button
+                  className="add-building"
+                  href="/adauga-cladire"
+                  icon="plus-circle"
+                  size="large"
+                >
+                  <span>
+                    <Trans>Add information</Trans>
+                  </span>
+                </Button>
+              </Col>
+            </Row>
+          </Col>
+          <Col>
+            <Icon type="close" onClick={onClose} style={{ color: '#EE4036' }} />
+          </Col>
+        </Row>
       ) : (
         <p>
           <Trans>Information missing</Trans>
