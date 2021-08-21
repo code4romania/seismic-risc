20b4ccd5b024cccbbd96629aab3c3d002138406c
Ciprian Platica <platica.ciprian@gmail.com>
2021-08-22 02:03:53 +0300

    Update SearchFragment to use Autocomplete

--- client/src/containers/home/SearchFragment.js
+++ client/src/containers/home/SearchFragment.js
@@ -1,18 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import { Row, Col, Input, Typography, message } from 'antd';
+import React, { useEffect, useState, useRef } from 'react';
+import { Row, Col, Typography, message, AutoComplete } from 'antd';
 import { Trans } from '@lingui/macro';
+import { debounce, groupBy } from 'lodash';
 
 import { ReactComponent as InfoIcon } from '../../images/info-circle-solid.svg';
 
 import { useGlobalContext } from '../../context';
 
-const { Search } = Input;
 const { Title } = Typography;
 
 export default () => {
   const {
     currentLanguage,
     searchBuildings,
+    searchResults,
     searchError,
     searchInput,
     onSearchInputChange,
@@ -37,6 +38,19 @@ export default () => {
     }
   }, [currentLanguage]);
 
+  const dataByStreet = groupBy(searchResults, (item) => item.general_id);
+  const dataSource = searchResults
+    ? searchResults.map((item) => {
+        return { value: item.general_id, text: `${item.address}, ${item.street_number}` };
+      })
+    : [];
+
+  const onSearch = useRef(debounce(searchBuildings, 1000)).current;
+
+  const onSelect = (value) => {
+    console.log('select', dataByStreet[value][0]);
+  };
+
   return (
     <Row
       type="flex"
@@ -55,12 +69,14 @@ export default () => {
           </Trans>
           :
         </Title>
-        <Search
+        <AutoComplete
+          allowClear
           value={searchInput}
-          loading={false}
+          dataSource={dataSource}
+          onChange={onSearchInputChange}
+          onSearch={onSearch}
+          onSelect={onSelect}
           placeholder={searchPlaceholderText}
-          onChange={(e) => onSearchInputChange(e.target.value)}
-          onSearch={(value) => searchBuildings(value)}
           style={{ width: '80%' }}
         />
       </Col>
