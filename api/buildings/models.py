18f465e03685b4a7db559745f08ec39ac047c28d
Tudor Amariei <tudor.amariei@code4.ro>
2021-09-25 12:50:51 +0300

    Implement building parent
    - add parent_id to a building
    - change the models outside of form to not be mandatory
    - change the order of building fields to match the form order
    - add necessary fields to the public_create endpoint

--- api/buildings/models.py
+++ api/buildings/models.py
@@ -94,6 +94,17 @@ class Building(models.Model):
     ]
 
     general_id = models.AutoField(_("general id"), primary_key=True)
+    parent_id = models.ForeignKey("self", models.SET_NULL, verbose_name=_("parent id"), blank=True, null=True)
+
+    status = models.SmallIntegerField(_("status"), default=PENDING, choices=BUILDING_STATUS_CHOICES, db_index=True)
+
+    street_number = models.CharField(_("street number"), max_length=100)
+    address = models.CharField(_("address"), max_length=250, null=True)
+    county = models.CharField(_("county"), max_length=60)
+    locality = models.CharField(_("locality"), max_length=20)
+
+    lat = models.FloatField(_("latitude"), null=True)
+    lng = models.FloatField(_("longitude"), null=True)
 
     risk_category = models.CharField(
         _("risk category"),
@@ -102,57 +113,7 @@ class Building(models.Model):
         default=SeismicCategoryChoice.NA,
         db_index=True,
     )
-    registration_number = models.IntegerField(_("registration number"), null=True)
-    examination_year = models.IntegerField(_("examination year"), null=True)
-    certified_expert = models.CharField(_("certified expert"), max_length=100, null=True)
-    observations = models.CharField(_("observations"), max_length=1000, null=True)
-
-    lat = models.FloatField(_("latitude"), null=True)
-    lng = models.FloatField(_("longitude"), null=True)
-
-    county = models.CharField(_("county"), max_length=60)
-    address = models.CharField(_("address"), max_length=250, null=True)
-    street_number = models.CharField(_("street number"), max_length=100)
-    locality = models.CharField(_("locality"), max_length=20)
-
-    year_built = models.IntegerField(_("year built"), null=True)
     height_regime = models.CharField(_("height regime"), max_length=50, null=True)
-    apartment_count = models.IntegerField(_("apartment count"), null=True, blank=True)
-    permanently_occupied_apartment_count = models.IntegerField(
-        _("permanently inhabited apartment count"), null=True, blank=True
-    )
-    rented_apartment_count = models.IntegerField(_("rented apartment count"), null=True, blank=True)
-    residents_count = models.IntegerField(_("resident count"), null=True, blank=True)
-    owners_count = models.IntegerField(_("owner count"), null=True, blank=True)
-    public_apartment_count = models.IntegerField(_("public apartment count"), null=True, blank=True)
-    public_owners = models.CharField(_("public owners"), null=True, blank=True, max_length=200)
-    surface = models.FloatField(_("surface"), null=True, blank=True)
-
-    has_owners_association = models.BooleanField(_("has owners association"), null=True, blank=True, default=None)
-    apartments_with_6_months_debt = models.IntegerField(_("6 month debt apartment count"), null=True, blank=True)
-    disconnected_utilities = models.CharField(_("disconnected utilities"), null=True, blank=True, max_length=200)
-    broken_utilities = models.CharField(_("broken utilities"), null=True, blank=True, max_length=200)
-
-    office_count = models.IntegerField(_("office count"), null=True, blank=True)
-    commercial_space_count = models.IntegerField(_("commercial space count"), null=True, blank=True)
-    self_owned_commercial_space_count = models.IntegerField(
-        _("self-owned commercial space count"), null=True, blank=True
-    )
-    proximal_utilities = models.ManyToManyField(
-        BuildingProximalUtilities, verbose_name=_("proximal utilities"), blank=True
-    )
-    proximal_utilities_description = models.CharField(
-        _("proximal utilities description"), null=True, blank=True, max_length=200
-    )
-
-    cadastre_number = models.IntegerField(_("cadastre number"), null=True, blank=True)
-    land_registry_number = models.CharField(_("land registry number"), max_length=50, null=True, blank=True)
-    administration_update = models.DateField(_("administration update"), null=True, blank=True)
-    admin_update = models.DateField(_("admin update"), null=True, blank=True)
-
-    status = models.SmallIntegerField(_("status"), default=PENDING, choices=BUILDING_STATUS_CHOICES, db_index=True)
-
-    created_on = models.DateTimeField(_("created on"), default=timezone.now, blank=True)
 
     is_still_present = models.BooleanField(_("is standing"), default=True, null=True, blank=True)
     consolidation_status = models.SmallIntegerField(_("is consolidated"), default=0, choices=CONSOLIDATION_CHOICES)
@@ -163,6 +124,51 @@ class Building(models.Model):
         through_fields=("building", "work_performed"),
         blank=True,
     )
+    apartment_count = models.IntegerField(_("apartment count"), null=True, blank=True)
+    permanently_occupied_apartment_count = models.IntegerField(
+        _("permanently inhabited apartment count"), null=True, blank=True
+    )
+    residents_count = models.IntegerField(_("resident count"), null=True, blank=True)
+    owners_count = models.IntegerField(_("owner count"), null=True, blank=True)
+    public_apartment_count = models.IntegerField(_("public apartment count"), null=True, blank=True)
+    public_owners = models.CharField(_("public owners"), null=False, default="", blank=True, max_length=200)
+    rented_apartment_count = models.IntegerField(_("rented apartment count"), null=True, blank=True)
+
+    has_owners_association = models.BooleanField(_("has owners association"), null=True, blank=True, default=None)
+    apartments_with_6_months_debt = models.IntegerField(_("6 month debt apartment count"), null=True, blank=True)
+    disconnected_utilities = models.CharField(
+        _("disconnected utilities"), null=False, default="", blank=True, max_length=200
+    )
+    broken_utilities = models.CharField(_("broken utilities"), null=False, default="", blank=True, max_length=200)
+
+    office_count = models.IntegerField(_("office count"), null=True, blank=True)
+    commercial_space_count = models.IntegerField(_("commercial space count"), null=True, blank=True)
+    self_owned_commercial_space_count = models.IntegerField(
+        _("self-owned commercial space count"), null=True, blank=True
+    )
+    proximal_utilities = models.ManyToManyField(
+        BuildingProximalUtilities, verbose_name=_("proximal utilities"), blank=True
+    )
+    proximal_utilities_description = models.CharField(
+        _("proximal utilities description"), null=False, default="", blank=True, max_length=200
+    )
+
+    registration_number = models.IntegerField(_("registration number"), null=True, blank=True)
+    examination_year = models.IntegerField(_("examination year"), null=True, blank=True)
+    certified_expert = models.CharField(_("certified expert"), max_length=100, null=False, default="", blank=True)
+    observations = models.CharField(_("observations"), max_length=1000, null=False, default="", blank=True)
+
+    year_built = models.IntegerField(_("year built"), null=True, blank=True)
+    surface = models.FloatField(_("surface"), null=True, blank=True)
+
+    cadastre_number = models.IntegerField(_("cadastre number"), null=True, blank=True)
+    land_registry_number = models.CharField(
+        _("land registry number"), max_length=50, null=False, default="", blank=True
+    )
+    administration_update = models.DateField(_("administration update"), null=True, blank=True)
+    admin_update = models.DateField(_("admin update"), null=True, blank=True)
+
+    created_on = models.DateTimeField(_("created on"), default=timezone.now, blank=True)
 
     objects = models.Manager()
     approved = ApprovedBuilding()
--- api/buildings/serializers.py
+++ api/buildings/serializers.py
@@ -27,14 +27,34 @@ class PublicBuildingCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Building
         fields = (
+            "parent_id",
             "street_number",
             "address",
             "county",
             "locality",
             "lat",
             "lng",
-            "height_regime",
             "risk_category",
+            "height_regime",
+            "is_still_present",
+            "consolidation_status",
+            "work_performed",
+            "apartment_count",
+            "permanently_occupied_apartment_count",
+            "residents_count",
+            "owners_count",
+            "public_apartment_count",
+            "public_owners",
+            "rented_apartment_count",
+            "has_owners_association",
+            "apartments_with_6_months_debt",
+            "disconnected_utilities",
+            "broken_utilities",
+            "office_count",
+            "commercial_space_count",
+            "self_owned_commercial_space_count",
+            "proximal_utilities",
+            "proximal_utilities_description",
         )
 
 
