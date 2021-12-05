f04af141d35f386ef2ae207df722789009299d72
Tudor Amariei <tudor.amariei@code4.ro>
2021-12-05 14:41:00 +0200

    Clean-up work
    - clean-up (some) errors, vulnerabilities, warnings, unused variables
    - update some easily update-able packages

--- api/buildings/serializers.py
+++ api/buildings/serializers.py
@@ -99,11 +99,17 @@ class BuildingSearchSerializer(BaseBuildingSerializer):
 
 class StatisticSerializer(serializers.ModelSerializer):
     evaluated_buildings = serializers.SerializerMethodField("get_total_buildings")
+    consolidated_buildings = serializers.SerializerMethodField("get_consolidated_buildings")
 
     @staticmethod
     def get_total_buildings(_):
         total_buildings = Building.approved.count()
         return int(total_buildings)
+
+    @staticmethod
+    def get_consolidated_buildings(_):
+        consolidated_buildings = Building.approved.filter(risk_category="C").count()
+        return int(consolidated_buildings)
 
     class Meta:
         model = Statistic
