9dac0b657c9da74bc662ae9af7c7d6175865691e
vladplesu <vladp1987@gmail.com>
2022-01-06 21:22:40 +0200

    fix blog post banner share icons

--- client/src/components/Sharing/index.js
+++ client/src/components/Sharing/index.js
@@ -1,5 +1,6 @@
 import React, { useCallback } from 'react';
 import { Trans } from '@lingui/macro';
+import { Col, Row } from 'antd';
 
 import copyToClipboardIcon from '../../images/copy-to-clipboard.svg';
 import facebookLogo from '../../images/facebook.svg';
@@ -15,47 +16,51 @@ function Sharing() {
   }, []);
 
   return (
-    <div className="sharing">
-      <Trans>Share:</Trans>
-      <a className="sharing-icon" onClick={copyToClipboard}>
-        <img src={copyToClipboardIcon} alt="" />
-      </a>
-      <a
-        className="sharing-icon"
-        target="_blank"
-        rel="noreferrer"
-        href={`https://www.facebook.com/sharer/sharer.php?u=${URL}`}
-      >
-        <img src={facebookLogo} alt="" />
-      </a>
-      <a
-        target="_blank"
-        rel="noreferrer"
-        className="sharing-icon"
-        href={`https://twitter.com/share?url=h${URL}`}
-        data-size="large"
-      >
-        <img src={twitterLogo} alt="" />
-      </a>
-      <a
-        className="sharing-icon"
-        target="_blank"
-        rel="noreferrer"
-        href={`https://wa.me/?text=${URL}`}
-        data-size="large"
-      >
-        <img src={whatsappLogo} alt="" />
-      </a>
-      <a
-        className="sharing-icon"
-        target="_blank"
-        rel="noreferrer"
-        href={`https://telegram.me/share/url?url=h${URL}`}
-        data-size="large"
-      >
-        <img src={telegramLogo} alt="" />
-      </a>
-    </div>
+    <Row className="sharing" type="flex" gutter={[10, 10]} align="middle">
+      <Col>
+        <Trans>Share:</Trans>
+      </Col>
+      <Col>
+        <a className="sharing-icon" onClick={copyToClipboard}>
+          <img src={copyToClipboardIcon} alt="" />
+        </a>
+        <a
+          className="sharing-icon"
+          target="_blank"
+          rel="noreferrer"
+          href={`https://www.facebook.com/sharer/sharer.php?u=${URL}`}
+        >
+          <img src={facebookLogo} alt="" />
+        </a>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          className="sharing-icon"
+          href={`https://twitter.com/share?url=h${URL}`}
+          data-size="large"
+        >
+          <img src={twitterLogo} alt="" />
+        </a>
+        <a
+          className="sharing-icon"
+          target="_blank"
+          rel="noreferrer"
+          href={`https://wa.me/?text=${URL}`}
+          data-size="large"
+        >
+          <img src={whatsappLogo} alt="" />
+        </a>
+        <a
+          className="sharing-icon"
+          target="_blank"
+          rel="noreferrer"
+          href={`https://telegram.me/share/url?url=h${URL}`}
+          data-size="large"
+        >
+          <img src={telegramLogo} alt="" />
+        </a>
+      </Col>
+    </Row>
   );
 }
 
