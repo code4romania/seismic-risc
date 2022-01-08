ed001d69ce20ea5d58d9b11fbd1da9dc0d9072f2
vladplesu <vladp1987@gmail.com>
2022-01-08 13:58:42 +0200

    fix undefined classNames add classnames package

--- client/src/components/Chip/index.js
+++ client/src/components/Chip/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 const Chip = ({ label, size }) => {
+  const chipClass = classNames('chip', size);
+
   return (
-    <div className={`chip ${size}`}>
+    <div className={chipClass}>
       <span>{label}</span>
     </div>
   );
