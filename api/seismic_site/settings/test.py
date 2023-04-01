8dc0706a0323e8095f7b6c1fc1e18b593929e3d2
Tudor Amariei <tudor.amariei@code4.ro>
2023-04-01 11:41:26 +0300

    Update the super-admin loading
    - modify the process so that there are default values in dev & test

--- api/seismic_site/settings/test.py
+++ api/seismic_site/settings/test.py
@@ -3,6 +3,12 @@ from seismic_site.settings.base import *
 DEBUG = True
 SECRET_KEY = "test_secret"
 SITE_URL = "http://localhost"
+
+SUPER_ADMIN_PASS = env("SUPER_ADMIN_PASS", default="pass")
+SUPER_ADMIN_EMAIL = env("SUPER_ADMIN_EMAIL", default="a@a.co")
+SUPER_ADMIN_FIRST_NAME = env("SUPER_ADMIN_FIRST_NAME", default="First")
+SUPER_ADMIN_LAST_NAME = env("SUPER_ADMIN_LAST_NAME", default="Last")
+
 EMAIL_BACKEND = "django.core.mail.backends.dummy.EmailBackend"
 
 TEST_RUNNER = "tests.runner.PytestTestRunner"
