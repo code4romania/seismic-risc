57a644c31082c238c90fe5bbea937eb679d86f23
Alina Mihai <alina_mihai90@yahoo.com>
2023-05-27 11:37:55 +0300

    fix lang dropdown
    and some theme issues

--- client/src/App.js
+++ client/src/App.js
@@ -27,15 +27,18 @@ i18n.activate('ro');
 
 function App() {
   return (
-    <ConfigProvider
-      theme={{
-        token: {
-          colorPrimary: '#EE4036',
-          '@font-family': '"Source Sans Pro", sans-serif',
-        },
-      }}
-    >
-      <I18nProvider i18n={i18n} forceRenderOnLocaleChange={false}>
+    <I18nProvider i18n={i18n} forceRenderOnLocaleChange={false}>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: '#EE4036',
+            fontFamily: '"Source Sans Pro", sans-serif',
+            colorLink: '#EE4036',
+            colorLinkHover: '#FA6E61',
+            colorText: '#000000A6',
+          },
+        }}
+      >
         <Router>
           <>
             <ScrollToTop />
@@ -53,8 +56,8 @@ function App() {
             </Routes>
           </>
         </Router>
-      </I18nProvider>
-    </ConfigProvider>
+      </ConfigProvider>
+    </I18nProvider>
   );
 }
 
