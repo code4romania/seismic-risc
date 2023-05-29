23fe5ce897922b71d4d3326d0a9b1aa2a8b1b27f
Alina Mihai <alina_mihai90@yahoo.com>
2023-05-29 10:12:34 +0300

    add content to hero story
    and remove invalid css rule

--- client/src/components/Hero/Hero.stories.js
+++ client/src/components/Hero/Hero.stories.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import heroImage from '../../images/home_hero.png';
 import Hero from '.';
 
-const Template = (args) => <Hero {...args} />;
+const Template = (args) => <Hero {...args} heroImage={heroImage} title="Title" />;
 
 export const Default = Template.bind({});
 Default.args = Hero.props;
