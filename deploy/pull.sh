d27f7c405595294beb14395c87e9896b610d8fdb
Tudor Amariei <tudor.amariei@code4.ro>
2023-05-30 12:21:48 +0300

    Update back-end requirements

--- deploy/pull.sh
+++ deploy/pull.sh
@@ -4,5 +4,5 @@ cd /root/seismic-risc
 git reset --hard HEAD
 git pull
 
-docker-compose build --build-arg ENVIRONMENT=development db api
+docker-compose build --build-arg ENVIRONMENT=development api
 docker-compose up -d db api
