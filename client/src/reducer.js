268b100d62946534a5700ade046c7731bb905a8f
Alina Mihai <alina_mihai90@yahoo.com>
2023-05-30 17:25:30 +0300

    fix reset search input field state

--- client/src/reducer.js
+++ client/src/reducer.js
@@ -62,7 +62,13 @@ const reducer = (state, action) => {
     }
 
     case 'CLEAR_SEARCH':
-      return { ...state, searchResults: [], searchError: null, searchInput: '' };
+      return {
+        ...state,
+        searchResults: [],
+        searchError: null,
+        searchInput: '',
+        searchSelectedBuilding: null,
+      };
 
     case 'RISK_CATEGORY_CHANGED':
       return { ...state, riskCategory: action.payload };
