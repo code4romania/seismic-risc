1ecfb7334547b7fc530e77868c1e1d7087d06e27
Dan Nicolae Mic <micdan1994@gmail.com>
2023-05-31 17:00:18 +0300

    [#885] Removed all logos from footer as well as CSS classses and styles applied to them.

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,11 +2,6 @@ import React from 'react';
 import { Layout, Typography } from 'antd';
 import { Trans } from '@lingui/macro';
 import { Link } from 'react-router-dom';
-import FooterLogo from './FooterLogo';
-
-import Mkbt from '../../images/MKBT-logo-black.png';
-import Commitglobal from '../../images/Commitglobal.svg';
-import Tfsg from '../../images/footer_tfsg.svg';
 
 import { useGlobalContext } from '../../context';
 
@@ -19,26 +14,6 @@ const FooterFragment = () => {
   return (
     <Footer className="footer">
       <div className="container">
-        <div className="footer__logos">
-          <FooterLogo
-            label={<Trans>A project by </Trans>}
-            href={`https://mkbt.ro/?lang=${currentLanguage}`}
-            src={Mkbt}
-            height="70px"
-          />
-          <FooterLogo
-            label={<Trans>Implemented by </Trans>}
-            href={`https://tfsg.code4.ro/${currentLanguage}`}
-            src={Tfsg}
-            height="50px"
-          />
-          <FooterLogo
-            label={<Trans>Designed by </Trans>}
-            href={`https://code4.ro/${currentLanguage}`}
-            src={Commitglobal}
-            height="50px"
-          />
-        </div>
         <div className="footer__links">
           <Link to="/adauga-cladire">
             <Text>
