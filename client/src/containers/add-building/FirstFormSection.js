2e4897184526bbae37ee6d917b6548f8260cb9fd
Alina Mihai <alina_mihai90@yahoo.com>
2023-06-07 14:44:54 +0300

    chore: comment out missing translation text

--- client/src/containers/add-building/FirstFormSection.js
+++ client/src/containers/add-building/FirstFormSection.js
@@ -108,11 +108,11 @@ const FirstFormSection = ({
             />
           </Col>
         </Row>
-        <Row>
+        {/* TODO add text for this translation id <Row>
           <Paragraph>
             <Trans id="form.first_section.extra" />
           </Paragraph>
-        </Row>
+        </Row> */}
       </Col>
       <Col xs={24} lg={10}>
         <HereMapAddBuilding
