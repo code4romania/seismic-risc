3667e2dc63658a6f8f243c0a44efa3e5f5a99465
Tudor Amariei <tudor.amariei@commitglobal.org>
2023-10-11 11:26:11 +0300

    Change deployment

--- deploy/pull.sh
+++ deploy/pull.sh
@@ -4,5 +4,5 @@ cd /root/seismic-risc
 git reset --hard HEAD
 git pull
 
-docker-compose build --build-arg ENVIRONMENT=development api
+docker-compose build --build-arg ENVIRONMENT=production api
 docker-compose up -d db api
