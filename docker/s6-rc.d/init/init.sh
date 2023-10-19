4ea0382c10064aae5b284de29daff49f804da9be
Daniel Ursache Dogariu <contact@danniel.net>
2023-10-19 08:01:21 +0300

    Improve shell boolean checking

--- docker/s6-rc.d/init/init.sh
+++ docker/s6-rc.d/init/init.sh
@@ -1,44 +1,70 @@
 #!/command/with-contenv bash
 
+
+# Convert one parameter to uppercase
+to_uppercase() {
+  echo $(echo $1 | tr '[:lower:]' '[:upper:]')
+}
+
+# Check if the parameter is string True/False and return it as success/failure
+is_enabled() {
+    if test "$(to_uppercase $1)" = "TRUE"; then
+        return 0
+    else
+        if test "$(to_uppercase $1)" = "FALSE"; then
+            return 1
+        else
+            echo "WARNING: init.sh interpreting \"$1\" as False" >/dev/stderr
+            return 1
+        fi    
+    fi
+}
+
+
 cd "${BACKEND_ROOT:-/var/www/seismic/api}" || exit 1
 
-echo "Checking..."
+echo "Running Django self-checks"
 python3 manage.py check
 
-if [ "${RUN_MIGRATION^^}" = "TRUE" ]; then
+# Run the database migrations
+if is_enabled "${RUN_MIGRATION:-False}"; then
     echo "Migrating database"
     python3 manage.py migrate --run-syncdb
     python3 manage.py createcachetable
 fi
 
-if [ "${RUN_COMPILE_MESSAGES^^}" = "TRUE" ]; then
+# Compile the translation messages
+if is_enabled ${RUN_COMPILE_MESSAGES:-False}; then
     echo "Compiling translation messages"
     python3 manage.py compilemessages
 fi
 
-if [ "${RUN_COLLECT_STATIC^^}" = "TRUE" ]; then
-    echo "Collect static"
+# Collect the static files
+if is_enabled ${RUN_COLLECT_STATIC:-False}; then
+    echo "Collecting static files"
     mkdir static
     python3 manage.py collectstatic --noinput
 fi
 
-if [ "${RUN_CREATE_SUPER_USER^^}" = "TRUE" ]; then
-    echo "Check superuser presence"
+# Create the Django Admin super user
+if is_enabled ${RUN_CREATE_SUPER_USER:-False}; then
+    echo "Checking superuser presence"
     SUPERUSERS=$(python3 manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); print(User.objects.filter(username=\"${DJANGO_ADMIN_EMAIL}\").count())")
 
-    if [ "${SUPERUSERS}" = "0" ]; then
-        echo "Create first superuser"
+    if test "${SUPERUSERS}" = "0"; then
+        echo "Creating first superuser"
         python3 manage.py createsuperuser --noinput --username "${DJANGO_ADMIN_EMAIL}"
 
-        echo "Set superuser password"
+        echo "Setting superuser password"
         python3 manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); u = User.objects.get(username=\"${DJANGO_ADMIN_EMAIL}\"); u.set_password(\"${DJANGO_ADMIN_PASSWORD}\"); u.save()"
     else
-        echo "A superuser already exists"
+        echo "A superuser already exists; nothing created"
     fi
 fi
 
-if [ "${RUN_LOAD_DUMMY_DATA^^}" = "TRUE" ] ; then
-  echo "Load dummy data into the database"
+# Load the dummy data
+if is_enabled ${RUN_LOAD_DUMMY_DATA:-False}; then
+  echo "Loading dummy data into the database"
   ./manage.py loaddata proximal_utilities
   ./manage.py loaddata work_performed
 fi
