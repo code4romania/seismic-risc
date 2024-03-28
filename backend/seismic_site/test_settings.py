254754338a20ad4367ab8512462ae96d88ff79ce
Tudor <tudor.amariei@commitglobal.org>
2024-03-28 11:02:11 +0200

    Fix automated tests (#970)

--- backend/seismic_site/test_settings.py
+++
@@ -1,9 +0,0 @@
-from seismic_site.settings import *  # noqa
-
-DEBUG = True
-SECRET_KEY = "test_secret"
-SITE_URL = "http://localhost"
-
-EMAIL_BACKEND = "django.core.mail.backends.dummy.EmailBackend"
-
-TEST_RUNNER = "tests.runner.PytestTestRunner"
