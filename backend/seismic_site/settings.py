735869bb86e21dad72c1aa51ede1326463043193
Daniel Ursache Dogariu <contact@danniel.net>
2024-03-30 13:01:32 +0200

    Drop unused settings

--- backend/seismic_site/settings.py
+++ backend/seismic_site/settings.py
@@ -309,12 +309,8 @@ MEDIA_URL = f"{public_media_location}/"
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, "static"))
 MEDIA_ROOT = os.path.abspath(os.path.join(BASE_DIR, "media"))
 
-DEV_DEPENDECIES_LOCATION = "bower_components"
+STATICFILES_DIRS = []
 
-STATICFILES_DIRS = [
-    os.path.abspath(os.path.join(DEV_DEPENDECIES_LOCATION)),
-    os.path.abspath(os.path.join("static_extras")),
-]
 
 default_storage_options = {}
 
