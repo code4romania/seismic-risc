2456573637640b157d1cad5c07c751ad00c6ff6e
Daniel Ursache Dogariu <contact@danniel.net>
2024-04-02 08:45:00 +0300

    SQL update the migrations table to fake-apply the custom User initial migration

--- docker/s6-rc.d/init/init.sh
+++ docker/s6-rc.d/init/init.sh
@@ -25,6 +25,13 @@ python3 manage.py check
 
 # Run the database migrations
 if is_enabled "${RUN_MIGRATIONS:-False}"; then
+    
+    # Fix the User model
+    echo "Fixing the User model"
+    echo "INSERT INTO django_migrations (app, name, applied) SELECT 'users', '0001_initial', CURRENT_TIMESTAMP WHERE NOT EXISTS (SELECT app FROM django_migrations WHERE app='users' AND name='0001_initial');" | python3 manage.py dbshell
+    echo "UPDATE django_content_type SET app_label = 'users' WHERE app_label = 'auth' and model = 'user';" | python manage.py dbshell
+    
+    # Run the actual migrations
     echo "Migrating database"
     python3 manage.py migrate --run-syncdb
     python3 manage.py createcachetable
